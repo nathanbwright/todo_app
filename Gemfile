80504367edff859da1510fff6af001d39a8c040d
nathanbwright <nathanielbwright@gmail.com>
2017-03-27 12:20:12 -0600

    Move to Haml

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 
 gem "sinatra", github: "sinatra/sinatra"
+gem "haml"
 
 group :development, :test do
   gem "rspec", "~> 3.5"
--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require "sinatra"
+require "haml"
 
 require_relative "lib/task"
 require_relative "lib/task_list"
 require_relative "lib/task_list_persistor"
 
 get "/" do
-  erb :index
+  haml :index, :locals => {:foo => "hello"}
+end
+
+post "/new-todo" do
+  @task =  params[:todo]
+  "This is what you entered #{@task}"
 end
