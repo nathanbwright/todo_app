b3f35fe0ab15d07cfd93f2747db4001b74c424c9
nathanbwright <nathanielbwright@gmail.com>
2017-03-28 08:03:43 -0600

    TaskApp can initialize from either a pre-existing file or an empty file

--- spec/task_app_spec.rb
+++ spec/task_app_spec.rb
@@ -32,4 +32,24 @@ RSpec.describe TaskApp do
     expect(subject.tasks.first.completed?).to be true
     expect(persistor).to have_received(:write).twice
   end
+
+  it "can initialize from a file" do
+    persistor = instance_double("TaskListPersistor")
+    task_list = TaskList.new("Example Title")
+    allow(persistor).to receive(:read).and_return(task_list)
+
+    subject = TaskApp.from_file(file_name: "example_file", persistor: persistor)
+
+    expect(subject.title).to eq("Example Title")
+  end
+
+  it "can initialize from an empty file" do
+    persistor = instance_double("TaskListPersistor")
+    allow(persistor).to receive(:read).and_return(nil)
+
+    subject = TaskApp.from_file(file_name: "example_file", persistor: persistor)
+
+    expect(subject.title).to eq("TaskApp")
+    expect(subject.tasks).to eq([])
+  end
 end
--- task_app.rb
+++ task_app.rb
@@ -5,6 +5,15 @@ require_relative "lib/task_list_persistor"
 class TaskApp
   attr_reader :title
 
+  def self.from_file(file_name:, task_list: TaskList, persistor: TaskListPersistor.new(file_name))
+    saved_task_list = persistor.read
+    if saved_task_list
+      new(title: saved_task_list.title, file_name: file_name, task_list: saved_task_list, persistor: persistor)
+    else
+      new(title: "TaskApp", file_name: file_name, task_list: task_list.new("TaskApp"), persistor: persistor)
+    end
+  end
+
   def initialize(title:, file_name:, task_list: TaskList.new(title), persistor: TaskListPersistor.new(file_name))
     @title = title
     @task_list = task_list
