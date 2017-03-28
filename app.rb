b6648c155b558c29c0c3bd95a3de626b85031539
nathanbwright <nathanielbwright@gmail.com>
2017-03-28 12:05:21 -0600

    Update app.rb with ability to add tasks and mark them as complete, add basic css styles

--- app.rb
+++ app.rb
@@ -1,15 +1,22 @@
 require "sinatra"
 require "haml"
 
-require_relative "lib/task"
-require_relative "lib/task_list"
-require_relative "lib/task_list_persistor"
+require_relative "./task_app"
+
+TASKS_FILE = "tasks.yaml"
+
+task_app = TaskApp.from_file(file_name: TASKS_FILE)
 
 get "/" do
-  haml :index, :locals => {:foo => "hello"}
+  haml :index, :locals => {:task_list_title => task_app.title, :task_list => task_app.tasks}
 end
 
 post "/new-todo" do
-  @task =  params[:todo]
-  "This is what you entered #{@task}"
+  task_app.add_task(params[:todo])
+  redirect back
+end
+
+post "/toggle_complete/:id" do
+  task_app.toggle_completed(params[:id])
+  redirect back
 end
