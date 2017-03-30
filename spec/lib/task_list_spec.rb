d0f72a66f39e82b03f02c2ccf2d8a6b47075fea0
nathanbwright <nathanielbwright@gmail.com>
2017-03-30 09:53:10 -0600

    Organize and clean up tests

--- spec/lib/task_list_spec.rb
+++ spec/lib/task_list_spec.rb
@@ -1,78 +1,75 @@
 require_relative "../../lib/task_list"
 
 RSpec.describe TaskList do
-  it "has a title" do
-    subject = TaskList.new("Example Title")
+  context "about itself" do
+    it "has a title" do
+      subject = TaskList.new("Example Title")
 
-    expect(subject.title).to eq("Example Title")
+      expect(subject.title).to eq("Example Title")
+    end
+
+    it "has a description" do
+      subject = TaskList.new("Example Title", "Example Description")
+
+      expect(subject.description).to eq("Example Description")
+    end
+
+    it "equality is based on state" do
+      subject = TaskList.new("Example Title", "Example Description")
+      other_subject = TaskList.new("Example Title", "Example Description")
+
+      subject.add_task("Example")
+      other_subject.add_task("Example")
+
+      expect(subject).to eq(other_subject)
+    end
   end
 
-  it "has a description" do
-    subject = TaskList.new("Example Title", "Example Description")
+  context "how it relates to tasks" do
+    let(:subject) { TaskList.new("Example Task") }
 
-    expect(subject.description).to eq("Example Description")
+    it "automatically creates Task objects" do
+      subject.add_task("Example Task")
+      task = subject.tasks.first
+
+      expect(task.title).to eq("Example Task")
+      expect(task.id).to eq(0)
+    end
+
+    it "can mark a task as completed" do
+      subject.add_task("Example Task")
+      subject.toggle_completed("0")
+
+      expect(subject.tasks.first.completed?).to be true
+    end
+
+    it "tasks can be found by id" do
+      subject.add_task("Example 0")
+      subject.add_task("Example 1")
+      subject.add_task("Example 2")
+
+      expect(subject.find_task_by_id("1").title).to eq("Example 1")
+    end
+
+    it "tasks can be deleted" do
+      subject.add_task("Task 0")
+      subject.delete_task("0")
+
+      expect(subject.tasks).to eq([])
+    end
+
+    it "all completed tasks can be deleted" do
+      subject.add_task("Task 0")
+      subject.add_task("Task 1")
+      subject.add_task("Task 2")
+      subject.toggle_completed("0")
+      subject.toggle_completed("2")
+
+      subject.delete_completed_tasks
+
+      expect(subject.tasks.first.title).to eq("Task 1")
+      expect(subject.tasks.size).to eq(1)
+    end
   end
 
-  it "automatically creates Task objects" do
-    subject = TaskList.new("Example Title")
-
-    subject.add_task("Example Task")
-    expect(subject.tasks.first.title).to eq("Example Task")
-    expect(subject.tasks.first.id).to eq(0)
-  end
-
-  it "can mark a task as completed" do
-    subject = TaskList.new("Example Title")
-
-    subject.add_task("Example Task")
-    subject.toggle_completed("0")
-
-    expect(subject.tasks.first.completed?).to be true
-  end
-
-  it "tasks can be found by id" do
-    subject = TaskList.new("Example Title")
-
-    subject.add_task("Example 0")
-    subject.add_task("Example 1")
-    subject.add_task("Example 2")
-
-    expect(subject.find_task_by_id(1).title).to eq("Example 1")
-  end
-
-  it "tasks can be deleted" do
-    subject = TaskList.new("Example Title")
-
-    subject.add_task("Task 0")
-    subject.delete_task("0")
-
-    expect(subject.tasks).to eq([])
-  end
-
-  it "all completed tasks can be deleted" do
-    subject = TaskList.new("Example Title")
-
-    subject.add_task("Task 0")
-    subject.toggle_completed("0")
-
-    subject.add_task("Task 1")
-
-    subject.add_task("Task 2")
-    subject.toggle_completed("2")
-
-    subject.delete_completed_tasks
-
-    expect(subject.tasks.first.title).to eq("Task 1")
-    expect(subject.tasks.size).to eq(1)
-  end
-
-  it "equality is based on state" do
-    subject = TaskList.new("Example Title", "Example Description")
-    other_subject = TaskList.new("Example Title", "Example Description")
-
-    subject.add_task("Example")
-    other_subject.add_task("Example")
-
-    expect(subject).to eq(other_subject)
-  end
 end
--- spec/task_app_spec.rb
+++ spec/task_app_spec.rb
@@ -1,83 +1,73 @@
 require_relative "../task_app"
 
 RSpec.describe TaskApp do
-  it "initialized with a file name and a title" do
-    persistor = instance_double("TaskListPersistor")
-    subject = TaskApp.new(title: "Task App", file_name: "file_name", persistor: persistor)
 
-    expect(subject.title).to eq("Task App")
+  context "about itself" do
+    let(:persistor) { instance_double("TaskListPersistor") }
+
+    it "initialized with a file name and a title" do
+      subject = TaskApp.new(title: "Task App", file_name: "file_name", persistor: persistor)
+      expect(subject.title).to eq("Task App")
+    end
+
+    it "can initialize from a file" do
+      task_list = TaskList.new("Example Title")
+      allow(persistor).to receive(:read).and_return(task_list)
+
+      subject = TaskApp.from_file(file_name: "example_file", persistor: persistor)
+
+      expect(subject.title).to eq("Example Title")
+    end
+
+    it "can initialize from an empty file" do
+      allow(persistor).to receive(:read).and_return(nil)
+
+      subject = TaskApp.from_file(file_name: "example_file", persistor: persistor)
+
+      expect(subject.title).to eq("TaskApp")
+      expect(subject.tasks).to eq([])
+    end
   end
 
-  it "automatically saves to disk when a task is added" do
-    persistor = spy("TaskListPersistor")
-    allow(persistor).to receive(:write)
+  context "automatically saves itself in different situations" do
+    let(:subject) { TaskApp.new(title: "Task App", file_name: "file_name", persistor: persistor) }
+    let(:persistor) { spy("TaskListPersistor") }
 
-    subject = TaskApp.new(title: "Task App", file_name: "file_name", persistor: persistor)
+    before(:each) do
+      allow(persistor).to receive(:write)
+    end
 
-    subject.add_task("Hello World")
+    it "when a task is added" do
+      subject.add_task("Hello World")
 
-    expect(subject.tasks.first.title).to eq("Hello World")
-    expect(persistor).to have_received(:write)
-  end
+      expect(subject.tasks.first.title).to eq("Hello World")
+      expect(persistor).to have_received(:write)
+    end
 
-  it "automatically saves to disk when a task is marked as completed" do
-    persistor = spy("TaskListPersistor")
-    allow(persistor).to receive(:write)
+    it "when a task is marked as completed" do
+      subject.add_task("Hello World")
+      subject.toggle_completed("0")
 
-    subject = TaskApp.new(title: "Task App", file_name: "file_name", persistor: persistor)
+      expect(subject.tasks.first.completed?).to be true
+      expect(persistor).to have_received(:write).twice
+    end
 
-    subject.add_task("Hello World")
-    subject.toggle_completed("0")
+    it "when a task is deleted" do
+      subject.add_task("Hello World")
+      subject.delete("0")
 
-    expect(subject.tasks.first.completed?).to be true
-    expect(persistor).to have_received(:write).twice
-  end
+      expect(subject.tasks).to eq([])
+      expect(persistor).to have_received(:write).twice
+    end
 
-  it "automatically saves to disk when a task is deleted" do
-    persistor = spy("TaskListPersistor")
-    allow(persistor).to receive(:write)
+    it "when all tasks are deleted" do
+      subject.add_task("Hello World")
+      subject.toggle_completed("0")
 
-    subject = TaskApp.new(title: "Task App", file_name: "file_name", persistor: persistor)
+      subject.delete_completed_tasks
 
-    subject.add_task("Hello World")
-    subject.delete("0")
-
-    expect(subject.tasks).to eq([])
-    expect(persistor).to have_received(:write).twice
-  end
-
-  it "automatically saves to disk when all tasks are deleted" do
-    persistor = spy("TaskListPersistor")
-    allow(persistor).to receive(:write)
-
-    subject = TaskApp.new(title: "Task App", file_name: "file_name", persistor: persistor)
-
-    subject.add_task("Hello World")
-    subject.toggle_completed("0")
-
-    subject.delete_completed_tasks
-
-    expect(subject.tasks).to eq([])
-    expect(persistor).to have_received(:write).thrice
-  end
-
-  it "can initialize from a file" do
-    persistor = instance_double("TaskListPersistor")
-    task_list = TaskList.new("Example Title")
-    allow(persistor).to receive(:read).and_return(task_list)
-
-    subject = TaskApp.from_file(file_name: "example_file", persistor: persistor)
-
-    expect(subject.title).to eq("Example Title")
-  end
-
-  it "can initialize from an empty file" do
-    persistor = instance_double("TaskListPersistor")
-    allow(persistor).to receive(:read).and_return(nil)
-
-    subject = TaskApp.from_file(file_name: "example_file", persistor: persistor)
-
-    expect(subject.title).to eq("TaskApp")
-    expect(subject.tasks).to eq([])
+      expect(subject.tasks).to eq([])
+      expect(persistor).to have_received(:write).thrice
+    end
   end
 end
