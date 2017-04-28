9f527a0fef5ab9f3b9c27732017d8787774188e5
nathanbwright <nathanielbwright@gmail.com>
2017-04-28 16:49:05 -0600

    Make task_app an instance variable of the Sinatra app

--- app.rb
+++ app.rb
@@ -4,35 +4,37 @@ require "haml"
 
 require_relative "./task_app"
 
-
 class App < Sinatra::Base
   register Sinatra::Partial
 
   TASKS_FILE = ARGV.first || "tasks.yaml"
 
-  task_app = TaskApp.from_file(file_name: TASKS_FILE)
+  def initialize
+    super
+    @task_app = TaskApp.from_file(file_name: TASKS_FILE)
+  end
 
   get "/" do
-    haml :index, :locals => {:task_app => task_app}
+    haml :index, :locals => {:task_app => @task_app}
   end
 
   post "/new-todo" do
-    task_app.add_task(params[:todo])
+    @task_app.add_task(params[:todo])
     redirect back
   end
 
   post "/toggle_complete/:id" do
-    task_app.toggle_completed(params[:id])
+    @task_app.toggle_completed(params[:id])
     redirect back
   end
 
   post "/delete/:id" do
-    task_app.delete(params[:id])
+    @task_app.delete(params[:id])
     redirect back
   end
 
   post "/delete_completed" do
-    task_app.delete_completed_tasks
+    @task_app.delete_completed_tasks
     redirect back
   end
 
