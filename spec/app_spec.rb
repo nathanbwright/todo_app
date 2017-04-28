361c9eb12aa184aed245917cfeabff684742380d
nathanbwright <nathanielbwright@gmail.com>
2017-04-28 15:09:45 -0600

    Remove RSpec app spec

--- spec/app_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.expand_path '../spec_helper.rb', __FILE__
-
-describe "Sinatra app" do
-
-  it "get the home page" do
-    get '/'
-    expect(last_response.body).to match(/Todo/)
-  end
-end
