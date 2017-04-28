a551103868b02375d2713a0105a6bdd42fd37eea
nathanbwright <nathanielbwright@gmail.com>
2017-04-28 15:47:28 -0600

    Make Sinatra app modular instead of classic

--- app.rb
+++ app.rb
@@ -1,33 +1,40 @@
-require "sinatra"
+require 'sinatra/base'
 require 'sinatra/partial'
 require "haml"
 
 require_relative "./task_app"
 
-TASKS_FILE = ARGV.first || "tasks.yaml"
 
-task_app = TaskApp.from_file(file_name: TASKS_FILE)
+class App < Sinatra::Base
+  register Sinatra::Partial
 
-get "/" do
-  haml :index, :locals => {:task_app => task_app}
-end
+  TASKS_FILE = ARGV.first || "tasks.yaml"
 
-post "/new-todo" do
-  task_app.add_task(params[:todo])
-  redirect back
-end
+  task_app = TaskApp.from_file(file_name: TASKS_FILE)
 
-post "/toggle_complete/:id" do
-  task_app.toggle_completed(params[:id])
-  redirect back
-end
+  get "/" do
+    haml :index, :locals => {:task_app => task_app}
+  end
 
-post "/delete/:id" do
-  task_app.delete(params[:id])
-  redirect back
-end
+  post "/new-todo" do
+    task_app.add_task(params[:todo])
+    redirect back
+  end
 
-post "/delete_completed" do
-  task_app.delete_completed_tasks
-  redirect back
+  post "/toggle_complete/:id" do
+    task_app.toggle_completed(params[:id])
+    redirect back
+  end
+
+  post "/delete/:id" do
+    task_app.delete(params[:id])
+    redirect back
+  end
+
+  post "/delete_completed" do
+    task_app.delete_completed_tasks
+    redirect back
+  end
+
+  run! if app_file == $0
 end
